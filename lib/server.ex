ebc6bc5a275ad14212b2e1d4a89b88474393d9d3
Mateusz Czarnecki <mateusz.czarnecki92@gmail.com>
2015-05-27 20:48:49 +0200

    Add Server skeleton and IndexedFile structure

---
+++ lib/server.ex
@@ -0,0 +1,27 @@
+defmodule IndexedFile do
+  defstruct id: 0, filename: "", folder: "", data: ''
+end
+
+defmodule WebFileIndexer.Server do
+  use GenServer
+
+  defmodule State do
+    defstruct files: []
+  end
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], [{:name, __MODULE__}])
+  end
+
+  def init([]) do
+    {:ok, %State{}}
+  end
+
+  def handle_call(request, _from, state) do
+
+  end
+
+  def handle_cast(_msg, state) do
+
+  end
+end
